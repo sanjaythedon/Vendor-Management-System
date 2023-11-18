538fef1c78a956d18006b050e0e25ba35da56bc4
Kokkiii <shanjairajan54@gmail.com>
2023-11-18 21:15:30 +0530

    basic model definition done

--- VMS/orders/models.py
+++ VMS/orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from vendors.models import Vendor
 
-# Create your models here.
+
+class PurchaseOrders(models.Model):
+    po_number = models.CharField(unique=True)
+    order_date = models.DateTimeField()
+    vendor = models.ForeignKey(to=Vendor, to_field='vendor_code', on_delete=models.CASCADE)
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    quantity = models.IntegerField()
+    status = models.CharField()
+    quality = models.FloatField()
+    issue_date = models.DateTimeField()
+    acknowledgment_date = models.DateTimeField()
--- VMS/vendors/models.py
+++ VMS/vendors/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=20)
+    contact_details = models.TextField()
+    address = models.TextField()
+    vendor_code = models.CharField(unique=True)
