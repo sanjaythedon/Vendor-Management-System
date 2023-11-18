3af637ca5f858e77f151763cc7f4cade9a559d96
Kokkiii <shanjairajan54@gmail.com>
2023-11-18 21:38:52 +0530

    models created (removed bugs)

--- VMS/orders/models.py
+++ VMS/orders/models.py
@@ -3,13 +3,13 @@ from vendors.models import Vendor
 
 
 class PurchaseOrders(models.Model):
-    po_number = models.CharField(unique=True)
+    po_number = models.CharField(unique=True, max_length=20)
     order_date = models.DateTimeField()
     vendor = models.ForeignKey(to=Vendor, to_field='vendor_code', on_delete=models.CASCADE)
     delivery_date = models.DateTimeField()
     items = models.JSONField()
     quantity = models.IntegerField()
-    status = models.CharField()
+    status = models.CharField(max_length=20)
     quality = models.FloatField()
     issue_date = models.DateTimeField()
     acknowledgment_date = models.DateTimeField()
